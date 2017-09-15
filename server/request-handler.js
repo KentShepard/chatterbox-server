725f0a661f026370f12add35d549254fe6167fce
Kent Shepard <kentshepard7@gmail.com>
2017-09-14 22:45:10 -0400

    Clean up code and remove comments from request handler

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,89 +1,36 @@
-/*************************************************************
-
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
 const querystring = require('querystring');
 const fs = require('fs');
 const url = require('url');
 
 let data = [];
 
-var defaultCorsHeaders = {
+const headers = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
+  'access-control-max-age': 10, // Seconds.
+  'Content-Type': 'JSON'
 };
 
 var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
   const { method, url } = request;
 
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
-  // console.log('Request', request);
-
-  var headers = defaultCorsHeaders;
-  headers['Content-Type'] = 'JSON';
-
-
 
   let handleGetMessages = function() {
-  // The outgoing status.
-    var statusCode = 200;
-
-    // See the note below about CORS headers.
-
-    // Tell the client we are sending them plain text.
-    //
-    // You will need to change this if you are sending something
-    // other than plain text, like JSON or HTML.
-
-    // .writeHead() writes to the request line and headers of the response,
-    // which includes the status and all headers.
+    let statusCode = 200;
     response.writeHead(statusCode, headers);
-    // console.log('GET request success', JSON.stringify({results: data}))
-    // Make sure to always call response.end() - Node may not send
-    // anything back to the client until you do. The string you pass to
-    // response.end() will be the body of the response - i.e. what shows
-    // up in the browser.
-    //
     if (data.length < 1) {
       response.end(JSON.stringify({results: [{username: 'Admin', text: 'Server is currently empty'}]}));
     } else {
       response.end(JSON.stringify({results: data}));
     }
-    // Calling .end "flushes" the response's internal buffer, forcing
-    // node to actually send all the data over to the client.
   };
 
   let handlePost = function () {
-    var statusCode = 201;
-    response.writeHead(statusCode, headers);
-
+    let statusCode = 201;
     let body = [];
-
+    response.writeHead(statusCode, headers);
     request.on('error', (err) => {
       console.error(err);
     });
@@ -120,28 +67,22 @@ var requestHandler = function(request, response) {
     response.writeHead(200, headers);
     response.end();
   };
-  // Example for sending css file
-  // =================================================================================
-  // if(/^\/[a-zA-Z0-9\/]*.css$/.test(request.url.toString())){
-  //    sendFileContent(response, request.url.toString().substring(1), "text/css");
-  // }
-  // example: /client/styles.css
+
   let dir = './chatterbox/hrr26-chatterbox-client/client';
 
-  function handleGetFiles(response, fileName, contentType){
-    fs.readFile(`${dir + fileName}`, function(err, data){
-      if(err){
+  let handleGetFiles = function(response, fileName, contentType) {
+    fs.readFile(`${dir + fileName}`, function(err, data) {
+      if (err) {
         response.writeHead(404);
-        response.write("Not Found!");
-      }
-      else{
+        response.write('Not Found!');
+      } else {
         response.writeHead(200, {'Content-Type': contentType});
         response.write(data);
       }
       response.end();
     });
-  }
-  // =================================================================================
+  };
+
   let allowedEndpoints = {
     '/classes/messages': true,
     '/': true,
@@ -158,9 +99,8 @@ var requestHandler = function(request, response) {
     'css': 'text/css',
     'json': 'application/json',
     'gif': 'image/gif'
-  }
+  };
 
-  // Decision Maker
   if (request.method === 'GET' && allowedEndpoints[request.url] || request.url.slice(0, 11) === '/?username=') {
     if (request.url === '/classes/messages') {
       handleGetMessages();
@@ -179,18 +119,5 @@ var requestHandler = function(request, response) {
     response.end('404 error', request.url);
   }
 };
-
-
-
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
-
 
 exports.requestHandler = requestHandler;
